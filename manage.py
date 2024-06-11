3d6b5357863e863e3dc27876ef075c999f175347
Yuji <g.yujinak@gmail.com>
2024-06-11 09:34:46 -0300

    alterações no código para python3.10

--- manage.py
+++ manage.py
@@ -3,6 +3,26 @@
 import os
 import sys
 
+import time
+from django.db import connections
+from django.db.utils import OperationalError
+
+def check_db_connection():
+    """Verifica a disponibilidade do banco de dados"""
+    db_conn = None
+    max_tries = 10
+    tries = 0
+    while not db_conn and tries < max_tries:
+        try:
+            db_conn = connections['default']
+            print("Database is available.")
+        except OperationalError:
+            tries += 1
+            print(f"Database unavailable, waiting 5 seconds... ({tries}/{max_tries})")
+            time.sleep(5)
+    if not db_conn:
+        print("Failed to connect to the database after several attempts.")
+        sys.exit(1)
 
 def main():
     """Run administrative tasks."""
@@ -15,8 +35,14 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
 
+    # Checar se estamos executando o comando wait_for_db
+    if len(sys.argv) > 1 and sys.argv[1] == "wait_for_db":
+        check_db_connection()
+    else:
+        execute_from_command_line(sys.argv)
 
 if __name__ == "__main__":
     main()
+
+
---
+++ wait-for-it.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# wait-for-it.sh
+# Use this script to test if a given TCP host/port are available
+# Source: https://github.com/vishnubob/wait-for-it
+
+set -e
+
+host="$1"
+shift
+port="$1"
+shift
+
+timeout="${WAIT_FOR_IT_TIMEOUT:-15}"
+cmd="$@"
+
+>&2 echo "Waiting for $host:$port..."
+
+for ((i=0;i<timeout;i++)); do
+    nc -z "$host" "$port" && break
+    >&2 echo "Waiting..."
+    sleep 1
+done
+
+>&2 echo "$host:$port is available"
+exec $cmd
